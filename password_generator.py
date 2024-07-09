32dd2405a5a2e1ac841dd83367c2deb5f279fe55
cosmic-cryp <deshmukhrutujeet@gmail.com>
2024-07-10 00:16:17 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -28,7 +28,7 @@ def password_generate():
 
 all_no = {"1" : "1", "2" : "2", "3" : "3", "4" : "4","5" : "5", "6" : "6", "7" : "7", "8" : "8", "9" : "9", "10" : "10", "11" : "11", "12" : "12", "13" : "13", "14" : "14", "15" : "15", "16" : "16", "17" : "17", "18" : "18", "19" : "19", "20" : "20", "21" : "21", "22" : "22", "23" : "23", "24" : "24", "25" : "25", "26" : "26", "27" : "27", "28" : "28", "29" : "29", "30" : "30"}
 
-Title = Label(root, text="Shravani Password Generator", bg="beige", fg="black", font=("futura", 15, "bold"))
+Title = Label(root, text="Password Generator", bg="beige", fg="black", font=("futura", 15, "bold"))
 Title.pack(anchor="center", pady="20px")
 
 length = Label(root, text="Select the Length of Your Password :- ", fg="darkgreen", bg="beige", font=("ubuntu", 12))
